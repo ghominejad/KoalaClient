11f033c778191c21e9ce2ebe23ebf2e7855898b2
Hassan Ghominejad <ghominejad@gmail.com>
2024-09-28 12:02:04 +0330

    Convert to js to fix compilation error

--- src/scripts/direction.js
+++ src/scripts/direction.js
@@ -1,28 +1,26 @@
-function isPersian(text: string): boolean {
+function isPersian(text) {
     // Persian Unicode range: \u0600-\u06FF
     const persianRegex = /[\u0600-\u06FF]/;
-    // Extract the first character
+    // Extract the first three characters
     const firstThreeChars = text.trim().substring(0, 1);
-    console.log(firstThreeChars);
+    console.log(firstThreeChars)
     // Test if any of the first three characters are Persian
     return persianRegex.test(firstThreeChars);
 }
-
-function setTextDirection(element: Element): void {
-    if (isPersian(element.textContent || "")) {
+function setTextDirection(element) {
+    if (isPersian(element.textContent)) {
         element.setAttribute('dir', 'rtl');
     } else {
         element.setAttribute('dir', 'ltr');
     }
 }
 
-const observer = new MutationObserver((mutations: MutationRecord[]) => {
+const observer = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
         mutation.addedNodes.forEach(node => {
             if (node.nodeType === Node.ELEMENT_NODE) {
-                const element = node as Element; // Type assertion to Element
-                if (['P', 'OL', 'UL', 'H3', 'H2', 'H1'].includes(element.tagName.toUpperCase())) {
-                    setTextDirection(element);
+                if (['P',  'OL', 'UL', "H3", "H2", "H1"].includes(node.tagName.toUpperCase())) {
+                    setTextDirection(node);
                 }
             }
         });
